2bdb4008bb64cfc17aea65952c2465296c47c457
Farooqfar <info4895@gmail.com>
2025-07-22 09:28:01 +0500

    push

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import router from "../routes/user.route.js";
-import serverless from "serverless-http";
+
 const server = express();
 server.use(
   cors({
@@ -14,6 +14,4 @@ server.use(cookieParser());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static("public"));
-server.use("/userapi", router);
-const handler = serverless(server);
-export { handler as default };
+server.use("/", router);
