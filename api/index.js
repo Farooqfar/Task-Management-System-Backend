aa9c1912f895f86ef7da27ad982c717e769def68
Farooqfar <info4895@gmail.com>
2025-07-22 09:15:28 +0500

    h

--- api/index.js
+++ api/index.js
@@ -14,6 +14,6 @@ server.use(cookieParser());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static("public"));
-server.use(router);
+server.use("/", router);
 const handler = serverless(server);
 export { handler as default };
