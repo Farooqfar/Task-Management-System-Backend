ec828a2da082eab11f1061515ca4cd34c50cc837
Farooqfar <info4895@gmail.com>
2025-07-22 10:28:37 +0500

    comit

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
-import app from "../server.js"; // make sure this path is correct
-import { createServer } from "http";
-import { parse } from "url";
+import app from "../server.js";
 
 export default function handler(req, res) {
-  const parsedUrl = parse(req.url, true);
-  req.url = parsedUrl.pathname;
-  app(req, res); // pass request to express app
+  return app(req, res);
 }
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import router from "./routes/user.route.js"; // adjust if needed
+import path from "path";
+import router from "./routes/user.route.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors({ origin: "*", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", router);
+app.use("/public", express.static(path.join(process.cwd(), "public")));
+app.use("/", router); // your routes
 
-export default app; // 👈 export the Express app
+export default app;
